13ffd625194a53931dd3d1963b6f306622fb9872
Vinay78696 <adityav7541@gmail.com>
2026-04-26 19:40:35 +0530

    verification test

--- src/main/java/com/projects/client_service/ClientServiceApplication.java
+++ src/main/java/com/projects/client_service/ClientServiceApplication.java
@@ -8,6 +8,7 @@ public class ClientServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientServiceApplication.class, args);
+		System.out.println("Hello");
 	}
 
 }
